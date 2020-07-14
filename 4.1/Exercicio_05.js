637df6ece5244237fe1c9f7634c4f5251bc0990d
Keila Sales <k.f.saless@gmail.com>
2020-07-13 22:33:08 -0300

    Exercício 5 do dia 4.1

---
+++ 4.1/Exercicio_05.js
@@ -0,0 +1,15 @@
+// Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. //
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário. //
+// Caso os ângulos estejam inválidos, o programa deve retornar uma mensagem de erro.// 
+
+let anguloA = 100;
+let anguloB = 50;
+let anguloC = 30;
+
+let anguloTotal = anguloA + anguloB + anguloC;
+
+if (anguloTotal === 180) {
+    console.log(true);
+} else if {
+    console.log(false);
+}
