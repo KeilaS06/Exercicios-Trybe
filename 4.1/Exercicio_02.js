6c0ab82e64931d72c778c786aa88ecb02deb39bd
Keila Sales <k.f.saless@gmail.com>
2020-07-13 21:36:00 -0300

    Exercício 2 do dia 4.1

---
+++ 4.1/Exercicio_02.js
@@ -0,0 +1,11 @@
+// Faça um programa que retorne o maior de dois números.//
+//Defina no começo do programa duas variáveis//
+
+let valor1 = 60;
+let valor2 = 40;
+
+if (valor1 > valor2) {
+    console.log("O maior número é " + valor1);
+} else {
+    console.log("O maior número é " + valor2);
+}
