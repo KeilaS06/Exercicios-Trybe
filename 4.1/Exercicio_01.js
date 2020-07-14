d1d3a83e6ab7a9288f2b33b27cf05c1ce645ff8d
Keila Sales <k.f.saless@gmail.com>
2020-07-13 21:17:00 -0300

    Exercicio 1 do dia 4.1

---
+++ 4.1/Exercicio_01.js
@@ -0,0 +1,32 @@
+// Seu programa deve ter duas variáveis //
+// Faça programas para adição, subtração, multiplicação, divisão e módulo.//
+
+let a = 5;
+let b = 10;
+
+// Programa p/ adição: //
+let soma;
+soma = a + b;
+console.log("O resultado é " + soma);
+
+// Programa p/ subtração: //
+let subtracao;
+subtração = a - b;
+console.log("O resultado é " + subtracao)
+
+// Programa p/ multiplicação: //
+let multiplicacao;
+multiplicação = a * b;
+console.log("O resultado é " + multiplicacao);
+
+// Programa p/ divisão //
+let divisao;
+divisao = a / b;
+console.log("O resultado é " + divisao);
+
+// Programa p/ módulo //
+let modulo;
+modulo = a % b;
+console.log("O resultado é " + modulo);
+
+
