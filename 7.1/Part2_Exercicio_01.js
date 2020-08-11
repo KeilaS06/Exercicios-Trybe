9d9fe1fe4cef7cc1557d5d876eecd7650bb0630c
Keila Sales <k.f.saless@gmail.com>
2020-08-11 19:11:39 -0300

    Exercicios da trybe

---
+++ 7.1/Part2_Exercicio_01.js
@@ -0,0 +1,15 @@
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const fatorial = num => {
+  if(num >0) {
+    let resultado = 1; // tem que ser 1 senão, não ele multiplica por 0
+    for(let i = 1; i <= num; i +=1) {
+      resultado *= i;
+    }
+    return resultado;
+  }
+}
+
+console.log(fatorial(5));
+
+
